965e33008e98b72b999621cedba6f8b1f4e07a77
thedekks <thedekkegor@gmail.com>
2026-03-08 15:04:20 +0300

    feat: parse flag

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main () {
 	fmt.Println("Hello, World!")
 
+	CurrentBranch := flag.String("cb", "", "Current Branch")
+	MainBranch := flag.String("mb", "main", "Main Branch")
+	CustomPromt := flag.Bool("cp", false, "Custom Promt")
+	OutFile := flag.String("o", "a.txt", "Out File")
 
+	flag.Parse()
+
+	fmt.Println(*CurrentBranch, *MainBranch, *CustomPromt, *OutFile)
 }
